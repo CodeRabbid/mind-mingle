68913a39e71f85d2a79d25bfa497d51d1c08f17c
Artiyom <artiyom.moskalew.2@gmail.com>
2024-06-06 19:25:22 +0200

    Add types to resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import "reflect-metadata";
+import { UserResolver } from "./UserResolver";
+import { buildSchema } from "type-graphql";
 
 (async () => {
   const app = express();
@@ -7,14 +10,9 @@ import { ApolloServer } from "apollo-server-express";
     res.send("hello");
   });
   const apolloServer = new ApolloServer({
-    typeDefs: `
-        type Query {
-            hello: String!
-        }
-    `,
-    resolvers: {
-      Query: { hello: () => "hello world" },
-    },
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
@@ -22,22 +20,3 @@ import { ApolloServer } from "apollo-server-express";
     console.log("express server started");
   });
 })();
-// AppDataSource.initialize()
-//   .then(async () => {
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await AppDataSource.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await AppDataSource.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log(
-//       "Here you can setup and run express / fastify / any other framework."
-//     );
-//   })
-//   .catch((error) => console.log(error));
