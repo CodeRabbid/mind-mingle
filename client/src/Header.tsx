e4485037bc2630db371b757c0bdcffdc6c63400a
Artiyom <artiyom.moskalew.2@gmail.com>
2024-06-17 14:11:27 +0200

    style header with orange backgordn and spread out navigation

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { useLogoutMutation, useMeQuery } from "./generated/graphql";
 import { setAccessToken } from "./accessToken";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./Header.css";
+import { FaArrowTrendUp, FaRegHourglassHalf, FaPlus } from "react-icons/fa6";
 
 const Header: React.FC = () => {
   const { data, loading } = useMeQuery();
@@ -12,42 +16,72 @@ const Header: React.FC = () => {
   if (loading) {
     body = <div>loading...</div>;
   } else if (data && data.me) {
-    body = <div>You are logged in as {data.me.email}</div>;
+    body = (
+      <Typography style={{ padding: "5px", color: "white" }}>
+        {" "}
+        {data.me.email}
+      </Typography>
+    );
   } else {
-    body = <div>not logged in</div>;
+    body = "";
   }
 
   return (
     <header>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <Link to="/register">Register</Link>
-      </div>
-      <div>
-        <Link to="/login">Login</Link>
-      </div>
-      <div>
-        <Link to="/posts">Posts</Link>
-      </div>
-      <div>
-        <Link to="/add-post">Add Post</Link>
-      </div>
-      <div>
-        {!loading && data && data.me && (
-          <button
-            onClick={async () => {
-              await logout();
-              setAccessToken("");
-              client.resetStore();
-            }}
-          >
-            Logout
-          </button>
+      <div id="header">
+        <div>
+          <Link to="/">
+            <Button style={{ color: "white" }}>
+              <FaRegHourglassHalf /> &nbsp;Recent posts
+            </Button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/posts">
+            <Button style={{ color: "white" }}>
+              <FaArrowTrendUp />
+              &nbsp; Trending
+            </Button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/add-post">
+            <Button style={{ color: "white" }}>
+              <FaPlus />
+              Add Post
+            </Button>
+          </Link>
+        </div>
+        {!data && (
+          <div>
+            <div style={{ float: "left" }}>
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </div>
+            <div style={{ float: "left" }}>
+              <Link to="/login">Login</Link>
+            </div>
+          </div>
         )}
+        <div>
+          <div style={{ float: "left" }}>{body}</div>
+          <div style={{ float: "left" }}>
+            {!loading && data && data.me && (
+              <Button
+                style={{ color: "white" }}
+                onClick={async () => {
+                  await logout();
+                  setAccessToken("");
+                  client.resetStore();
+                }}
+              >
+                Logout
+              </Button>
+            )}
+          </div>
+        </div>
       </div>
-      {body}
     </header>
   );
 };
